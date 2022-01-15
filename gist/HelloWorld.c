8a8141dbe74ebe8fdd14505a11d4c94ac9ad4eb9
OctopusLian <zoctopus@qq.com>
2022-01-16 00:10:21 +0800

    hello world

---
+++ gist/HelloWorld.c
@@ -0,0 +1,13 @@
+/*
+ * @Description: 
+ * @Author: neozhang
+ * @Date: 2022-01-04 22:39:41
+ * @LastEditors: neozhang
+ * @LastEditTime: 2022-01-15 23:56:09
+ */
+#include "stdio.h"
+int main(int argc, char const *argv[])
+{
+    printf("Hello World!\n");
+    return 0;
+}
